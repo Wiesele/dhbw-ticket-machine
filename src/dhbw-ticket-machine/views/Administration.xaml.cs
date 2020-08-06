13f25bd81b23e6f9c624f8cd019bb8e3db64ad06
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-06 14:35:09 +0200

    added views

---
+++ src/dhbw-ticket-machine/views/Administration.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dhbw_ticket_machine.views
+{
+    /// <summary>
+    /// Interaktionslogik für Administration.xaml
+    /// </summary>
+    public partial class Administration : Page
+    {
+        public Administration()
+        {
+            InitializeComponent();
+        }
+    }
+}
