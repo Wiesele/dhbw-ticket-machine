4ece3358428943015ad7141c8a5d866f5c187934
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-07 14:56:56 +0200

    Fixed Class Name

---
+++ src/dhbw-ticket-machine/Actors/AdministrationActor.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+using dhbw_ticket_machine.actors.TransactionTypes;
+using dhbw_ticket_machine.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dhbw_ticket_machine.Actors
+{
+    public class AdministrationActor: ReceiveActor
+    {
+        IDatabase database;
+        public AdministrationActor()
+        {
+            this.database = MockDatabase.Create();
+            Receive<TransactionType>(e =>
+            {
+                if(e == TransactionType.GetAll)
+                {
+                    Sender.Tell(this.database.Events);
+                }
+            });
+        }
+
+        public static Props Pros()
+        {
+            return Props.Create(() => new AdministrationActor());
+        }
+    }
+}
