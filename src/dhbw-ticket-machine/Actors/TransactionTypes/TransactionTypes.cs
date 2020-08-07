8a58d35f39c3bbcf2b9ce1d1fbb94cdd12430860
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-07 14:55:45 +0200

    Added Transaction Types

---
+++ src/dhbw-ticket-machine/Actors/TransactionTypes/TransactionTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dhbw_ticket_machine.actors.TransactionTypes
+{
+    public enum TransactionType
+    {
+        Get,
+        GetAll
+    }
+}
