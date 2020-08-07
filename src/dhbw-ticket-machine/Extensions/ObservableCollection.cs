560a72dbc11f5118a7d766b87942745a0cc1ec36
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-07 14:57:35 +0200

    Added Externsion Methods for ObservableCollections

---
+++ src/dhbw-ticket-machine/Extensions/ObservableCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace dhbw_ticket_machine.Extensions
+{
+    public static class ObservableCollection
+    {
+        public static void AddRange<T>(this ObservableCollection<T> say, IEnumerable<T> Items)
+        {
+            foreach (var item in Items)
+            {
+                say.Add(item);
+            }
+        }
+    }
+}
