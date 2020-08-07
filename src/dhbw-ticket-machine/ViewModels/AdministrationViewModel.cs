714b0752b6557305bc40d8996dd4d493f37e0902
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-07 14:56:45 +0200

    Added View Models

---
+++ src/dhbw-ticket-machine/ViewModels/AdministrationViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using System.Text;
+using dhbw_ticket_machine.Data;
+using dhbw_ticket_machine.Extensions;
+using dhbw_ticket_machine.Models;
+
+namespace dhbw_ticket_machine.ViewModels
+{
+    public class AdministrationViewModel:BindableBase
+    {
+        private ObservableCollection<Event> _events;
+        public ObservableCollection<Event> Events { get { return this._events; } set { SetProperty(ref _events, value); } }
+
+        public AdministrationViewModel()
+        {
+            this.Events = new ObservableCollection<Event>();
+            //this.Events. ser.Events;
+        }
+
+        public void LoadData()
+        {
+            var ser = MockDatabase.Create();
+            this.Events.AddRange(ser.Events);
+        }
+    }
+}
---
+++ src/dhbw-ticket-machine/ViewModels/BindableBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Controls;
+
+namespace dhbw_ticket_machine.ViewModels
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool SetProperty<T>(ref T field, T value, [CallerMemberName] string ProperyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            RaisePropertyChanged(ProperyName);
+            return true;
+        }
+        private void RaisePropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
