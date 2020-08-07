a8f682ccd4b25f2e96463a573004b1d7e72d6f8a
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-07 16:22:50 +0200

    Added Value Converter

---
+++ src/dhbw-ticket-machine/Converter/DateConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace dhbw_ticket_machine.Converter
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var seperates = value.ToString().Split(".");
+            DateTime time;
+            try
+            {
+                time = new DateTime(Int32.Parse(seperates[2]), Int32.Parse(seperates[1]), Int32.Parse(seperates[0]));
+            }
+            catch
+            {
+                time = DateTime.Now;
+            }
+            return time;
+        }
+    }
+}
