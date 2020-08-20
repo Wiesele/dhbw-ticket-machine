8c00cbfd722cb72c26384c6ef62a49b0c15e3cb9
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-20 13:14:41 +0200

    Added sorting to bought tickets

--- src/dhbw-ticket-machine/Views/CustomerViews/ShowTickets.xaml.cs
+++ src/dhbw-ticket-machine/Views/CustomerViews/ShowTickets.xaml.cs
@@ -3,6 +3,7 @@ using dhbw_ticket_machine.Models;
 using dhbw_ticket_machine.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -52,6 +53,79 @@ namespace dhbw_ticket_machine.Views.CustomerViews
         private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             this.vm.FilterEvents();
+        }
+
+
+        /// <summary>
+        /// Source for sorting 
+        /// https://stackoverflow.com/a/9746174
+        /// https://docs.microsoft.com/de-de/dotnet/framework/wpf/controls/how-to-sort-a-gridview-column-when-a-header-is-clicked
+        /// </summary>
+        private GridViewColumnHeader _lastHeaderClicked = null;
+        private ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
+        private void listView_Click(object sender, RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null)
+            {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+                {
+                    if (headerClicked != _lastHeaderClicked)
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        if (_lastDirection == ListSortDirection.Ascending)
+                        {
+                            direction = ListSortDirection.Descending;
+                        }
+                        else
+                        {
+                            direction = ListSortDirection.Ascending;
+                        }
+                    }
+
+                    var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+                    var sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+
+                    Sort(sortBy, direction);
+
+                    if (direction == ListSortDirection.Ascending)
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowUp"] as DataTemplate;
+                    }
+                    else
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowDown"] as DataTemplate;
+                    }
+
+                    // Remove arrow from previously sorted header
+                    if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+                    {
+                        _lastHeaderClicked.Column.HeaderTemplate = null;
+                    }
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+        }
+
+        private void Sort(string sortBy, ListSortDirection direction)
+        {
+            ICollectionView dataView =
+              CollectionViewSource.GetDefaultView(listView.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
         }
     }
 }
