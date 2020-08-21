7398584177d16baa60a81c6f637cfb34d85ab6f2
Marvin Rothmann <m.rothmann@fburi.de>
2020-08-21 13:21:16 +0200

    added menu in customer view

--- src/dhbw-ticket-machine/Views/CustomerMain.xaml.cs
+++ src/dhbw-ticket-machine/Views/CustomerMain.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using dhbw_ticket_machine.Views.CustomerViews;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,6 +27,41 @@ namespace dhbw_ticket_machine.Views
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             mainFrame.NavigationService.Navigate(new SelectCustomer());
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Windows.OfType<MainWindow>().Any())
+            {
+                var window = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+
+                if(window.WindowState == WindowState.Minimized)
+                {
+                    window.WindowState = WindowState.Normal;
+                }
+                window.Activate();
+            }
+            else
+            {
+                var window = new MainWindow();
+                window.Show();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(Application.Current.Windows.Count < 2)
+            {
+                var msg = MessageBox.Show("Die Anwendung wird beendet. Alle geänderten Daten gehen verloren!\n\n Fortfahren?", "Warnung!", MessageBoxButton.YesNo);
+                if (msg == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+                else if (msg == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
         }
     }
 }
